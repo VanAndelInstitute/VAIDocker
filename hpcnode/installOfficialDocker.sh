242f7b3d22e5a9f2ac2d2228918034fec25d36f7
zackramjan <zack.hr@gmail.com>
2022-03-31 12:07:44 -0400

    added nvidia docker toolkit

--- hpcnode/installOfficialDocker.sh
+++ hpcnode/installOfficialDocker.sh
@@ -9,6 +9,10 @@ yum -y install containerd.io.x86_64
 yum -y install docker-ce
 curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x  /usr/local/bin/docker-compose
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID) 
+curl -s -L https://nvidia.github.io/libnvidia-container/$distribution/libnvidia-container.repo | sudo tee /etc/yum.repos.d/nvidia-container-toolkit.repo
+yum clean expire-cache
+yum install -y nvidia-docker2
 systemctl enable docker
 systemctl start docker
 
