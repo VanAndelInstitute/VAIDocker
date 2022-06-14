8fa7c647b5d2ebce4d40f41a30a3469daf4c1634
Zack Ramjan <zack.hr@gmail.com>
2022-06-14 14:49:40 -0400

    detect ip change and get new keys on stop/start

--- bbcnode_aws/bounce.sh
+++ bbcnode_aws/bounce.sh
@@ -6,3 +6,12 @@ su - ec2-user -c "echo mounting studies"
 docker-compose restart
 sleep 5
 docker exec rstudio_bbc systemctl start systemd-user-sessions
+
+
+#set up the cert and proxy
+docker stop caddy
+docker rm caddy
+echo `curl ifconfig.me`.nip.io { > /root/Caddyfile
+echo "   reverse_proxy localhost:8080" >> /root/Caddyfile
+echo } >> /root/Caddyfile
+docker run -d --name caddy --network host -v /root/Caddyfile:/etc/caddy/Caddyfile caddy
