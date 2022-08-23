b39791804a346e8ccbb652f713f9f0a5d99592b1
zackramjan <zack.hr@gmail.com>
2022-08-23 16:43:50 -0400

    set rstudio user pass and display

--- bbcnode_aws/bounce.sh
+++ bbcnode_aws/bounce.sh
@@ -15,3 +15,14 @@ echo `curl ifconfig.me`.nip.io { > /root/Caddyfile
 echo "   reverse_proxy localhost:8080" >> /root/Caddyfile
 echo } >> /root/Caddyfile
 docker run -d --name caddy --network host -v /root/Caddyfile:/etc/caddy/Caddyfile caddy
+
+
+#DISPLAY user password and URL
+instance_id=$(curl -s "http://169.254.169.254/latest/meta-data/instance-id")
+instance_region=$(curl -s "http://169.254.169.254/latest/meta-data/placement/region")
+PASSWORD=`aws ssm get-parameter --name "/rstudio/user/sc-environments/ec2-instance/${instance_id}"  --region $instance_region --with-decryption --output text | cut -f 7`
+MYIP=`curl ifconfig.me 2> /dev/null`
+echo "You can log into RSTUDIO SERVER at the following URL:" > /etc/motd
+echo  https://$MYIP.nip.io  >> /etc/motd
+echo  username: ec2-user      password: $PASSWORD  >> /etc/motd
+echo  password: $PASSWORD  >> /etc/motd
--- bbcnode_aws/start.sh
+++ bbcnode_aws/start.sh
@@ -33,7 +33,18 @@ echo "   reverse_proxy localhost:8080" >> /root/Caddyfile
 echo } >> /root/Caddyfile
 docker run -d --name caddy --network host -v /root/Caddyfile:/etc/caddy/Caddyfile caddy
 
-
+#set the password and display URL to user on login
+PASSWORD=`tr -dc A-Za-z0-9 </dev/urandom | head -c 13 ; echo ''`
+instance_id=$(curl -s "http://169.254.169.254/latest/meta-data/instance-id")
+instance_region=$(curl -s "http://169.254.169.254/latest/meta-data/placement/region")
+aws ssm put-parameter --name "/rstudio/user/sc-environments/ec2-instance/${instance_id}" --value $PASSWORD --region $instance_region --type SecureString --no-overwrite 2> /dev/null
+PASSWORD=`aws ssm get-parameter --name "/rstudio/user/sc-environments/ec2-instance/${instance_id}"  --region $instance_region --with-decryption --output text | cut -f 7`
+#echo $PASSWORD | passwd --stdin ec2-user
+MYIP=`curl ifconfig.me 2> /dev/null`
+echo "You can log into RSTUDIO SERVER at the following URL:" > /etc/motd
+echo  https://$MYIP.nip.io  >> /etc/motd
+echo  username: ec2-user      password: $PASSWORD  >> /etc/motd
+echo  password: $PASSWORD  >> /etc/motd
 
 #WITHIN CONTAINER (NOT USED)
 #sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*
