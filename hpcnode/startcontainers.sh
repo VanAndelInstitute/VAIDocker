94a92ebcacd8b502a33a2b9a73e0f99da273ace1
zackramjan <zack.hr@gmail.com>
2023-02-01 00:27:44 -0500

    use dockerhub instead of local builds

--- hpcnode/startcontainers.sh
+++ hpcnode/startcontainers.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+cd /varidata/research/admin/VAIDocker/hpcnode
 cmd="docker-compose  -f $HOSTNAME-docker-compose.yml up -d"
 echo $cmd
 $cmd
--- hpcnode/stopcontainers.sh
+++ hpcnode/stopcontainers.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+cd /varidata/research/admin/VAIDocker/hpcnode
 cmd="docker-compose  -f $HOSTNAME-docker-compose.yml down"
 echo $cmd
 $cmd
