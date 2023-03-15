fbcbcda922e3d44ad5358e907fdee5be996fa5e1
zackramjan <zack.hr@gmail.com>
2023-03-14 21:56:16 -0400

    tool to sync dockerfile yum pkgs to live containers

--- hpcnode/syncDockerfileToRunningContainers.pl
+++ hpcnode/syncDockerfileToRunningContainers.pl
@@ -4,43 +4,41 @@ use File::Basename;
 
 my $EXECPATH = abs_path(dirname(__FILE__));
 my $dockerFile = do{local(@ARGV,$/)="$EXECPATH/Dockerfile";<>};
+$SIG{INT} = \&tsktsk;
+$start = shift @ARGV || 1;
+$end = shift @ARGV || 254;
 
 #test if git repo is up to date
 
 chdir($EXECPATH);
-$gitOutput =  `git fetch --dry-run`;
+$gitOutput =  `git fetch --dry-run;git diff Dockerfile`;
 chomp $gitOutput;
-die "This git repo is not up to date, please pull/fetch any changes from remote\n" if $gitOutput;
+die "This git repo is not up to date, please push any local to master, or pull/fetch any changes from remote\n" if $gitOutput;
 
 $dockerFile =~ s/\\\n/ /g;
 print STDERR "running yum install of following:\n";
+my @commands;
 while ($dockerFile =~ /(yum\s+install\s.+?)(\n|\&)/gm)
 {
-	print "\t$1\n";
-
+	push @commands, $1;
 }
-
-
-
-exit;
-$SIG{INT} = \&tsktsk;
-
-$start = shift @ARGV || 1;
-$end = shift @ARGV || 254;
 
 for my $n ($start..$end)
 {
-    $n = "00$n" if ($n <= 9);
-    $n = "0$n" if ($n >= 10 && $n < 100);
-
-	print STDERR "compute$n:\n";
-	system("ssh root\@compute$n \"" . join(" ",@ARGV) . "\"" );
-	print  "\n";
-	sleep 1;
+	my $ip = "10.152.223.$n";
+	runcmd("ssh root\@$ip \"" . join(";",@commands) . "\"" );
 }
 
 
 sub tsktsk {
       $SIG{INT} = \&tsktsk;           # See ``Writing A Signal Handler''
       exit 0;
+}
+sub runcmd{
+	my $date = `date`;
+	chomp $date;
+        my $cmd=shift @_;
+        my $caller=(caller(1))[3];
+        print STDERR "$date\t$caller\t$cmd\n";
+	system($cmd);
 }
