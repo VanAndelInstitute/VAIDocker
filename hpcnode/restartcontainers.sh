9fedd0d5ea12efe723f6dc13ec4ea58429e6a001
zackramjan <zack.hr@gmail.com>
2023-03-16 11:13:20 -0400

    updated mem limits. added restart script

---
+++ hpcnode/restartcontainers.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd /varidata/research/clustermgmt/vaihpc/VAIDocker/hpcnode
+cmd="docker-compose  -f $HOSTNAME-docker-compose.yml down"
+echo $cmd
+$cmd
+
+cd /varidata/research/clustermgmt/vaihpc/VAIDocker/hpcnode
+cmd="docker-compose  -f $HOSTNAME-docker-compose.yml up -d"
+echo $cmd
+$cmd
