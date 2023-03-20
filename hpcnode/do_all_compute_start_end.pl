4270715fdb472162fe44ec2e594cd0fa340673dc
zackramjan <zack.hr@gmail.com>
2023-03-20 13:20:20 -0400

    added email support via postfix

---
+++ hpcnode/do_all_compute_start_end.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/perl
+#
+$SIG{INT} = \&tsktsk;
+
+$start = shift @ARGV || 5;
+$end = shift @ARGV || 5;
+sub tsktsk {
+      $SIG{INT} = \&tsktsk;           # See ``Writing A Signal Handler''
+      exit 0;
+        }
+
+for my $n ($start..$end)
+{
+    $n = "00$n" if ($n <= 9);
+    $n = "0$n" if ($n >= 10 && $n < 100);
+
+	print STDERR "compute$n:\n";
+	system("ssh root\@compute$n \"" . join(" ",@ARGV) . "\"" );
+	print  "\n";
+	sleep 1;
+}
+
