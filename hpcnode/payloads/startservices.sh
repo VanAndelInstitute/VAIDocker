1bcaf0f158de397e99b737f80acd264cc6e435cb
zackramjan <zack.hr@gmail.com>
2023-04-19 15:32:54 -0400

    lowered mem

--- hpcnode/payloads/startservices.sh
+++ hpcnode/payloads/startservices.sh
@@ -9,7 +9,7 @@ cp /varidata/research/clustermgmt/vaihpc/munge/munge.key /etc/munge
 chown munge /etc/munge/munge.key
 systemctl start	munge
 
-sleep 60
+sleep 20
 if [ "$HOSTNAME" =  "slurm" ]; then
     #SLURMCTLD (We are starting the master)
     systemctl start slurmdbd
