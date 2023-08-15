fc6709adf5f6c4250aa1e8957cc77d0a9a0a6485
zackramjan <zack.hr@gmail.com>
2023-08-15 15:11:54 -0400

    added libpq-devel to dockerfile

--- hpcnode/payloads/fixTmp.pl
+++ hpcnode/payloads/fixTmp.pl
@@ -6,8 +6,8 @@ $mounts =~ /tmpfs \((.+)\)$/;
 exit unless $1;
 $mountArgs = $1;
 $mountArgs =~ s/noexec/exec/g;
-
 system "mount -o remount -o$mountArgs /tmp";
+print STDERR  "mount -o remount -o$mountArgs /tmp\n";
 
 
 
