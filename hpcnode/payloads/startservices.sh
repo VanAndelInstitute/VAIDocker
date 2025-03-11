0b4ac249255d7b81ed8cd0d6ddf0ef1a4a2ed18a
zackramjan <zack.hr@gmail.com>
2025-03-11 12:39:19 -0400

    handle /etc/environment when compute node is a gpu node

--- hpcnode/payloads/startservices.sh
+++ hpcnode/payloads/startservices.sh
@@ -7,6 +7,11 @@ systemctl start systemd-user-sessions
 #fix tmp noexec to exec
 perl /root/fixTmp.pl
 
+#set /etc/environment if we have a gpunode
+if [ -d "/proc/driver/nvidia" ]; then
+	cat /etc/environment.gpunode >> /etc/environment
+fi
+
 #MUNGE
 cp /varidata/research/clustermgmt/vaihpc/munge/munge.key /etc/munge
 chown munge /etc/munge/munge.key
@@ -14,7 +19,7 @@ systemctl start	munge
 
 
 sleep 20
-if [ "$HOSTNAME" =  "slurm" ]; then
+if [ "$HOSTNAME" =~  "slurm" ]; then
     #SLURMCTLD (We are starting the master)
     systemctl enable slurmdbd
     systemctl start slurmdbd
