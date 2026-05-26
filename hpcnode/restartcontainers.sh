719362248b197b3b8fc9ef4cfe7dfe35decc6f15
zackramjan <zack.hr@gmail.com>
2026-05-26 11:45:05 -0400

    move to docker compose instead of docker-compose

--- hpcnode/restartcontainers.sh
+++ hpcnode/restartcontainers.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 cd /varidata/research/clustermgmt/vaihpc/VAIDocker/hpcnode
-cmd="docker-compose  -f $HOSTNAME-docker-compose.yml down"
+cmd="docker compose  -f $HOSTNAME-docker-compose.yml down"
 echo $cmd
 $cmd
 
 cd /varidata/research/clustermgmt/vaihpc/VAIDocker/hpcnode
-cmd="docker-compose  -f $HOSTNAME-docker-compose.yml up -d"
+cmd="docker compose  -f $HOSTNAME-docker-compose.yml up -d"
 echo $cmd
 $cmd
 
--- hpcnode/startcontainers.sh
+++ hpcnode/startcontainers.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-cd /varidata/research/clustermgmt/vaihpc/VAIDocker/hpcnode
-cmd="docker-compose  -f $HOSTNAME-docker-compose.yml up -d"
-echo $cmd
-$cmd
+atLeastOneContainerUp=$(docker ps | tail -n +2 | tr -d '\n')
+if [[ $atLeastOneContainerUp != "" ]]; then
+	echo "There is at least one container running already. Use restartContainers instead."
+else
+	echo "No containers currently running. Attempting start."
+	cd /varidata/research/clustermgmt/vaihpc/VAIDocker/hpcnode
+	cmd="docker compose  -f $HOSTNAME-docker-compose.yml up -d"
+	echo $cmd
+	$cmd
+fi
